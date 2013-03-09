3e8573297898c2fee0294f09bad03569bdacbddb
Katarzyna Rutkowska <krutkowska@sigma.ug.edu.pl>
2013-03-09 12:39:41 +0100

    Pierwsza, surowa wersja aplikacyjki my_gists

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyGists::Application.config.secret_token = 'd5575c1beb7692c14a3560f1cc8574142ad5e938f644179174c3a74475b12e3fd86c77efdcedafc509a3b0a5513df8cf00c185ab717830b97cf79dd6ea876772'
