f142eb9af9e1050a924a6ba22e9541c83844ba8d
Katarzyna Rutkowska <krutkowska@sigma.ug.edu.pl>
2013-03-15 21:13:52 +0100

    wykorzystanie bootstrapa do stylizacji projektu

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,3 +1,7 @@
 class Gist < ActiveRecord::Base
   attr_accessible :description, :lexer, :snippet
+  #ograniczenie - pola lexer i snippet muszą być wypełnione
+  validates_presence_of :lexer, :message => "must be provided"
+  validates_presence_of :snippet, :message => "must be provided"
 end
+
