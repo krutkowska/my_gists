c0ba99e873b35d28ee1e60f516208c799ef37b4d
Katarzyna Rutkowska <krutkowska@sigma.ug.edu.pl>
2013-04-06 09:16:47 +0200

    ustawienie routingu

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 MyGists::Application.routes.draw do
   devise_for :users
-
-  resources :gists 
-  
+ resources :gists
+ 
+root :to => 'gists#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
